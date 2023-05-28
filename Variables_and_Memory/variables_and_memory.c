16b2fbaf6b34348f8c2b62c6a27e0d79a2279137
efraimvis <efraimvishnivetzky@gmail.com>
2023-05-28 11:16:43 +0300

    Added powerpoint slide 'variables_and_memory_structure.pptx'

--- Variables_and_Memory/variables_and_memory.c
+++ Variables_and_Memory/variables_and_memory.c
@@ -2,28 +2,35 @@
 
 int global_int; //This is a global variable
 
+/**
+ * @brief Tries to swap a,b. Will not work,
+ *        because the FUNCTION's a,b are LOCAL
+ *        to it, and don't have the same ADDRESSES
+ *        as the a,b passed to it. therefore, it will
+ *        swap its own a,b, but not affect what was passed
+ *        to it. 
+ * 
+ * @param first First number
+ * @param second Second number
+ */
+void local_int_swap_wrong(int first, int second)
+{
+    int temp = second;
+    second = first;
+    first = temp;    
+}
 
 /**
- * @brief Adds 5 to a given number.
+ * @brief Demonstrates value persistence of local static variables.
  * 
- * @param n Number to which to add 5.
  */
-int local_int_add_five(int n)
+void local_static_variable()
 {
-    int five = 5; // This LOCAL VARIABLE exists only as long as this function is running.
-    int result = n + five;
-
-    printf("Addresses of `local_int_add_five`'s variables:\n\
-            `n`:\t%p\n\
-            `five`:\t%p\n\
-            `result`:\t%p\n", 
-            &n,
-            &five,
-            &result);
-
-    printf("Result is: %d\n", result);
-    return result;
+    static int static_int;
+    printf("The ADDRESS of `static_int` is:\t%p\n", &static_int);
+    printf("The VALUE of `static_int` is:\t%d\n", static_int++);
 }
+
 
 int main(void)
 {
@@ -54,30 +61,54 @@ int main(void)
     //A variable will be allocated an amount of memory in one of a NUMBER OF DIFFERENT
     //DESIGNATED SECTIONS of the program's memory, depending on the variable's SCOPE and LIFETIME;
 
+    printf("******************************VARIABLE SCOPE AND LIFETIME:********************************\n");
     //A variable's SCOPE is the part of the program in which the variable
     //"exists" or is recognized:
 
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~LOCAL VARIABLES:~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     //LOCAL variables are defined within a function or block, and are destroyed once the program exits that scope.
     //LOCAL variables are stored in the program memory's STACK, within the STACK FRAME of their respective
     //function or block. Let's take a look at an example:
-    int n = 5;
-    int result;
-    printf("Addresses of `main`'s variables:\n\
-            `n`:\t%p\n\
-            `result`:\t%p\n",
-            &n,
-            &result);
-    result = local_int_add_five(n);
-    printf("Result is: %d\n", result);
+    int first = 5;
+    int second = -5;
+    local_int_swap_wrong(a,b);
+    printf("`main`'s a,b values:\n\
+            \t%d\n\
+            \t%d\n", first, second);
 
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~GLOBAL VARIABLES:~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     //GLOBAL variables are defined OUTSIDE of functions or blocks, and can be accessed from ANYWHERE in the program.
     //They are created when the program starts, and continue to exist until it terminates.
     //GLOBAL variables are stored in the program memory's DATA SEGMENT.
 
+    //Let's look at the address of the GLOBAL variable `global_int` defined above:
+    printf("Address of GLOBAL variable `global_int`: %p\n", &global_int);
+
+    //Since `global_int` is a GLOBAL VARIABLE, we can access and change it FROM ANYWHERE in the program.
+    //Let's see this in action:
+    printf("Value of GLOBAL variable `global_int`, "
+           "before change: %d\n", global_int);
+    global_int = 123;
+    printf("Value of GLOBAL variable `global_int`, "
+           "after change: %d\n", global_int);
+
+    //We can see that the address of the GLOBAL variable `global_int` is relatively far away from the
+    //addresses of our `main` function's LOCAL variables, which gives us a clue that it is in fact
+    //stored in a different segment of the program's memory.
+
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~STATIC VARIABLES:~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     //STATIC variables are defined either as LOCAL or GLOBAL variables, and once they are created - they persist
     //until the program terminates. For example, if a static variable is created in a function, the next time
     //that function is called, the variable will have the value that it had in the previous call.
     //STATIC variables are stored in the program memory's DATA SEGMENT, like global variables.
+
+    //Let's take a look at a STATIC VARIABLE defined in the function `local_static_variable` implemented above:
+    local_static_variable();
+    local_static_variable();
+    local_static_variable();
+
+
+    const int const_int = 47;
 
     //DYNAMICALLY ALLOCATED variables can be defined anywhere in the program, and the memory allocated to them
     //continues to be allocated to them (and therefore cannot be allocated to other variables) until
@@ -86,13 +117,6 @@ int main(void)
 
     //The variables `a`,`b`,`c` and `d` are LOCAL variables, and we can see that their addresses
     //are relatively close toghether - since they exist on the same STACK FRAME.
-
-    //Let's look at the address of the GLOBAL variable `global_int` defined above:
-    printf("Address of GLOBAL variable `global_int`: %p\n", &global_int);
-
-    //We can see that the address of the GLOBAL variable `global_int` is relatively far away from the
-    //addresses of our `main` function's LOCAL variables, which gives us a clue that it is in fact
-    //stored in a different segment of the program's memory.
 
     return 0;
 }
